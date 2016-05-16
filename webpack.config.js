e2fa6a1dfdd20123a6936bfd3ecaa5927712a907
ovidiu <ovidiu.bute@gmail.com>
2016-05-16 10:25:25 +0300

    Fixed source map breaking change in Chrome.

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ const common = {
 
 if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
-    devtool: 'eval-source-map',
+    devtool: '#eval-source-map',
     devServer: {
       contentBase: PATHS.build,
 
